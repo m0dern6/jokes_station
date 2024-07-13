94d7e6c93bce1c8320c60213e6710c27a9b34252
m0dern6 <na.ab.en.153@gmail.com>
2024-07-14 05:17:54 +0545

    splash screen with a animation

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    splashFunc();
+  }
+
+  splashFunc() async {
+    await Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Lottie.asset('assets/splash.json'),
+    );
+  }
+}
